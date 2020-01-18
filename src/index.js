0ae0624fff1554f9ea3f5b4966d752002ae182c9
Guillaume <deleuzegui@hotmail.com>
2020-01-18 18:08:15 +0100

    add Dashboard

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { Provider } from "react-redux";
 import configureStore from "./store";
 import "./index.css";
 import App from "./App";
+import Dashboard from "./components/dashboard/Dashboard";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <Dashboard />
   </Provider>,
   document.getElementById("root")
 );
